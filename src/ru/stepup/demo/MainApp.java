f33d73969af375c0249e198f7919a1017e390a7e
PaulSPuntusov <pps_ru@mail.ru>
2024-06-24 00:04:08 +0300

    убрал печать 1 и 2

--- src/ru/stepup/demo/MainApp.java
+++ src/ru/stepup/demo/MainApp.java
@@ -2,10 +2,11 @@ package ru.stepup.demo;
 
 public class MainApp {
     public static void main(String[] args){
-        System.out.println(1);
-        System.out.println(2);
+
+
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
+
     }
 }
